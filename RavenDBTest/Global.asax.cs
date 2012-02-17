07196123f941e0a97b935d66029be2060b7ca2fc
Michael Friis <friism@gmail.com>
2012-02-17 01:02:41 -0800

    dont hardcode

--- RavenDBTest/Global.asax.cs
+++ RavenDBTest/Global.asax.cs
@@ -26,12 +26,11 @@ namespace RavenDBTest
 			RegisterRoutes(RouteTable.Routes);
 
 			Store = new DocumentStore
-			{
-				ApiKey = "33f81f9a-111f-4cec-83c3-f0c4afb9a5ef",
-				Url = "https://1.ravenhq.com/databases/AppHarbor_6566a44b-959e-4766-9c58-ab9000d54d49"
-			};
-
-			//{ ConnectionStringName = "RavenDB" };
+			//{
+			//    ApiKey = "33f81f9a-111f-4cec-83c3-f0c4afb9a5ef",
+			//    Url = "https://1.ravenhq.com/databases/AppHarbor_6566a44b-959e-4766-9c58-ab9000d54d49"
+			//};
+			{ ConnectionStringName = "RavenDB" };
 			Store.Initialize();
 		}
 	}
